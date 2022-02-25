4f7bec0e705f06b5228455b63165d7141eaf55be
Ederson Ferreira <ederson.dev@gmail.com>
2022-02-25 18:49:27 -0300

    setting routes for sync component

--- frontend/src/app/order/order.module.ts
+++ frontend/src/app/order/order.module.ts
@@ -1,15 +1,22 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SyncComponent } from './sync/sync.component';
 
-
+const routes:Routes = [
+  {
+    path: '',
+    component: SyncComponent
+  }
+];
 
 @NgModule({
   declarations: [
     SyncComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class OrderModule { }
