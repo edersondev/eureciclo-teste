6b51fdb388d9ad8cedde7e499976a5b0c11cc18e
Ederson Ferreira <ederson.dev@gmail.com>
2022-02-25 18:43:52 -0300

    added order module and sync component

---
+++ frontend/src/app/order/order.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SyncComponent } from './sync/sync.component';
+
+
+
+@NgModule({
+  declarations: [
+    SyncComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class OrderModule { }
