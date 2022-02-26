9f2f6677ffbec8c5dd5309b4c13a13d3656be9c0
Ederson Ferreira <ederson.dev@gmail.com>
2022-02-26 19:47:42 -0300

    created method convertCsvToArray

--- backend/app/Http/Service/SyncOrderService.php
+++ backend/app/Http/Service/SyncOrderService.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Service;
 
+use Illuminate\Support\Str;
+
 class SyncOrderService
 {
 
@@ -11,6 +13,24 @@ class SyncOrderService
   public function syncData($request)
   {
     $path = $request->file('ordercsv')->store('sync_orders');
+    
+    $arrayData = $this->convertCsvToArray($path);
+
+    dd($arrayData);
   }
 
+  private function convertCsvToArray($path)
+  {
+    $fullPath = storage_path("app/{$path}");
+    $arrayFromCsv = array_map(function($v){return str_getcsv($v, "\t");}, file($fullPath));
+
+    $header = array_map(function($v){
+      return Str::slug($v);
+    },array_shift($arrayFromCsv));
+
+    return array_map(function($v) use ($header) {
+      return array_combine($header,$v);
+    },$arrayFromCsv);
+
+  }
 }
